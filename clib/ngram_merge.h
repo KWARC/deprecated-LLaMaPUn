06785a2ad93aec794259f8de3361f79099832808
jfschaefer <j.schaefer@jacobs-university.de>
2014-07-02 17:50:05 +0200

    removed ngram_merge (because unlikely to ever be used)

--- clib/ngram_merge.h
+++
@@ -1,8 +0,0 @@
-#ifndef NGRAM_MERGE_H
-#define NGRAM_MERGE_H
-//#include <json-c/json.h>
-#include "jsoninclude.h"
-
-void ngram_merge_push(json_object * jobj);
-json_object *pull_merged_ngrams();
-#endif
