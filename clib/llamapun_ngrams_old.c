46a38ac020370a92e9d18db0ee884b781aa12d51
jfschaefer <j.schaefer@jacobs-university.de>
2014-07-24 12:55:04 +0200

    use dnmlib for ngram search :)

--- clib/llamapun_ngrams_old.c
+++ clib/llamapun_ngrams_old.c
@@ -9,13 +9,17 @@
 // JSON
 //#include <json-c/json.h>
 #include "jsoninclude.h"
+// XML DOM and XPath
+#include <libxml/tree.h>
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
+#include <libxml/xpathInternals.h>
 // LLaMaPUn Utils
 #include "llamapun_utils.h"
 #include "stopwords.h"
 #include "stemmer.h"
-#include "llamapun_ngrams.h"
+#include "llamapun_ngrams_old.h"
 #include "unicode_normalizer.h"
-#include "dnmlib.h"
 
 
 struct stringcount {
@@ -24,13 +28,72 @@ struct stringcount {
   UT_hash_handle hh;
 };
 
+/*void add_ngram(const char *string, struct stringcount *sc) {  //does't work (macros??)
+  //FILE *f = fopen("log.txt", "a");
+  struct stringcount *tmp;
+  HASH_FIND_STR(sc, string, tmp);
+  if (tmp == NULL) {
+    tmp = (struct stringcount *) malloc(sizeof(struct stringcount));
+    tmp->string = strdup(string);
+    tmp->counter = 1;
+    HASH_ADD_KEYPTR(hh, sc, tmp->string, strlen(tmp->string), tmp);
+  } else {    //ngram is already in hash
+    tmp->counter++;
+  }
+}*/
 
-json_object* llamapun_get_ngrams (xmlDocPtr doc) {
+json_object* llamapun_get_ngrams_old (xmlDocPtr doc) {
   char* log_message;
   if (doc == NULL) {
     fprintf(stderr, "Failed to parse workload!\n");
-    log_message = "Failed to parse document.";
-    return cortex_response_json("",log_message,-4);
+    log_message = "Fatal:LibXML:parse Failed to parse document.";
+    return cortex_response_json("",log_message,-4); }
+  xmlXPathContextPtr xpath_context = xmlXPathNewContext(doc);
+  if(xpath_context == NULL) {
+    fprintf(stderr,"Error: unable to create new XPath context\n");
+    xmlFreeDoc(doc);
+    log_message = "Fatal:LibXML:XPath unable to create new XPath context\n";
+    return cortex_response_json("",log_message,-4); }
+
+  /* Register XHTML, MathML namespaces */
+  xmlXPathRegisterNs(xpath_context,  BAD_CAST "xhtml", BAD_CAST "http://www.w3.org/1999/xhtml");
+  xmlXPathRegisterNs(xpath_context,  BAD_CAST "m", BAD_CAST "http://www.w3.org/1998/Math/MathML");
+  xmlNsPtr xhtml_ns = xmlNewNs(xmlDocGetRootElement(doc),BAD_CAST "http://www.w3.org/1999/xhtml",BAD_CAST "xhtml");
+  if (xhtml_ns == NULL) {
+    perror("Failed to create XHTML namespace");  }
+
+  xmlChar *sentence_xpath = (xmlChar*) "//xhtml:span[@class='ltx_sentence']";
+  /* Generically stem all "math" elements to "[MathFormula]" words */
+  xmlChar* math_xpath = (xmlChar*) "//m:math";
+  xmlXPathObjectPtr xpath_math_result = xmlXPathEvalExpression(math_xpath,xpath_context);
+  if (xpath_math_result != NULL) { // Nothing to do if there's no math in the document
+    xmlNodeSetPtr math_nodeset = xpath_math_result->nodesetval;
+    int math_index;
+    for (math_index=0; math_index < math_nodeset->nodeNr; math_index++) {
+      xmlNodePtr math_node = math_nodeset->nodeTab[math_index];
+      xmlNodePtr new_math_word = xmlNewNode(xhtml_ns, BAD_CAST "span");
+      xmlNewProp(new_math_word, BAD_CAST "class", BAD_CAST "ltx_word");
+      xmlNodePtr math_stub_text = xmlNewText(BAD_CAST "[MathFormula]");
+      xmlSetProp(new_math_word,BAD_CAST "id", BAD_CAST xmlGetProp(math_node,BAD_CAST "id"));
+      xmlAddChild(new_math_word,math_stub_text);
+      xmlReplaceNode(math_node,new_math_word);
+    }
+  }
+  /* Stem all "a" anchor elements to their text content */
+  xmlChar* anchor_xpath = (xmlChar*) "//xhtml:a";
+  xmlXPathObjectPtr xpath_anchor_result = xmlXPathEvalExpression(anchor_xpath,xpath_context);
+  if (xpath_anchor_result != NULL) { // Nothing to do if there's no math in the document
+    xmlNodeSetPtr anchor_nodeset = xpath_anchor_result->nodesetval;
+    int anchor_index;
+    for (anchor_index=0; anchor_index < anchor_nodeset->nodeNr; anchor_index++) {
+      xmlNodePtr anchor_node = anchor_nodeset->nodeTab[anchor_index];
+      xmlNodePtr new_anchor_word = xmlNewNode(xhtml_ns, BAD_CAST "span");
+      xmlNewProp(new_anchor_word, BAD_CAST "class", BAD_CAST "ltx_word");
+      xmlNodePtr anchor_text = xmlNewText(BAD_CAST xmlNodeGetContent(anchor_node));
+      xmlSetProp(new_anchor_word,BAD_CAST "id", BAD_CAST xmlGetProp(anchor_node,BAD_CAST "id"));
+      xmlAddChild(new_anchor_word,anchor_text);
+      xmlReplaceNode(anchor_node,new_anchor_word);
+    }
   }
 
   //initialize everything for counting ngrams
@@ -38,30 +101,51 @@ json_object* llamapun_get_ngrams (xmlDocPtr doc) {
   struct stringcount *unigram_hash = NULL;
   struct stringcount * bigram_hash = NULL;
   struct stringcount *trigram_hash = NULL;
-
+  //if (access("../stopwords.json", R_OK)) {
+  //  fprintf(stderr, "Error: Cannot open \"../stopwords.json\"\n");
+  //  return NULL;
+  //}
+  //read_stopwords_from_json(json_object_from_file("../stopwords.json"));
   load_stopwords();
 
-  dnmPtr dnm = createDNM(doc, DNM_NORMALIZE_MATH | DNM_SKIP_CITE);
-  dnmIteratorPtr it_sent = getDnmIterator(dnm, DNM_LEVEL_SENTENCE);
+  //Evaluate XPath - CAUSES OCCASIONALLY SEG FAULTS - bug in libxml?
+  xmlXPathObjectPtr xpath_sentence_result = xmlXPathEvalExpression(sentence_xpath, xpath_context);
 
+  if(xpath_sentence_result == NULL) {
+
+    fprintf(stderr,"Error: unable to evaluate sentence xpath expression \"%s\"\n", sentence_xpath);
+    xmlXPathFreeContext(xpath_context);
+    xmlFreeDoc(doc);
+    log_message = "Fatal:LibXML:XPath unable to evaluate xpath expression\n";
+    return cortex_response_json("",log_message,-4); }
+
+  /* Sentence nodes: */
+  xmlNodeSetPtr sentences_nodeset = xpath_sentence_result->nodesetval;
+  /* Traverse each sentence and tag words */
+  xmlChar *words_xpath = (xmlChar*) "//xhtml:span[@class='ltx_word']";
+  int sentence_index;
   //loop over sentences
-  do {
-    int isnumber;
-    size_t tmpindex;
-    dnmIteratorPtr it_word = getDnmChildrenIterator(it_sent);
-    if (it_word == NULL) //no words
-      continue;
-
-
+  for (sentence_index=0; sentence_index < sentences_nodeset->nodeNr; sentence_index++) {
+    xmlNodePtr sentence = sentences_nodeset->nodeTab[sentence_index];
+    xmlXPathContextPtr xpath_sentence_context = xmlXPathNewContext((xmlDocPtr)sentence);
+    xmlXPathRegisterNs(xpath_sentence_context,  BAD_CAST "xhtml", BAD_CAST "http://www.w3.org/1999/xhtml");
+    /* We want a list of words */
+    xmlXPathObjectPtr words_xpath_result = xmlXPathEvalExpression(words_xpath, xpath_sentence_context);
+    if(words_xpath_result == NULL) { // No words, skip
+      xmlXPathFreeContext(xpath_sentence_context);
+      continue; }
+    xmlNodeSetPtr words_nodeset = words_xpath_result->nodesetval;
+    char* word_input_string;
     size_t sentence_size;
     FILE *word_stream;
-    char *word_input_string;
     word_stream = open_memstream (&word_input_string, &sentence_size);
-
-    //loop over words
-    do {
-      char* word_content = getDnmIteratorContent(it_word);
-
+    int word_count = words_nodeset->nodeNr;
+    int words_index;
+    int isnumber;
+    size_t tmpindex;
+    for (words_index=0; words_index < word_count; words_index++) {
+      xmlNodePtr word_node = words_nodeset->nodeTab[words_index];
+      char* word_content = (char*) xmlNodeGetContent(word_node);
       //add word_content to word_stream (replace numbers by "[number]")
       isnumber = 1;
       tmpindex = 0;
@@ -77,13 +161,10 @@ json_object* llamapun_get_ngrams (xmlDocPtr doc) {
       } else {
         fprintf(word_stream, "%s", word_content);
       }
-      if (it_word->pos < it_word->end - 1) fprintf(word_stream, " ");   //no trailing space!!
+      if (words_index < word_count - 1) fprintf(word_stream," ");   //no trailing space!!
+    }
 
-      free(word_content);
-    } while (dnmIteratorNext(it_word));
-
-    free(it_word);
-
+    //xmlElemDump(word_stream,doc,sentence);
     fclose(word_stream);
 
     //prepare for ngram extraction
@@ -96,6 +177,13 @@ json_object* llamapun_get_ngrams (xmlDocPtr doc) {
     int foundStopword;
     int foundEnd = 0;
     int nonstopcounter;
+
+/*
+    FILE *f = fopen("log.txt", "a");
+    //fprintf(f, " +++ NEW SENTENCE +++ ");
+    fprintf(f, "%s", word_input_string);
+    fclose(f);
+*/
 
     //do the actual ngram extraction (iteratively)
     do {
@@ -201,14 +289,12 @@ json_object* llamapun_get_ngrams (xmlDocPtr doc) {
     } while (!foundEnd);
     free(word_input_string);
     free(stemmed);
-
-  } while (dnmIteratorNext(it_sent));
-
-  free(it_sent);
-  freeDNM(dnm);
+  }
 
   close_stemmer();
   free_stopwords();
+  xmlXPathFreeContext(xpath_context);
+
 
   //now transfer data to json object
   json_object* response = json_object_new_object();
---
+++ clib/llamapun_ngrams_old.h
@@ -0,0 +1,11 @@
+// JSON
+//#include <json-c/json.h>
+#include "jsoninclude.h"
+// XML DOM and XPath
+#include <libxml/tree.h>
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
+#include <libxml/xpathInternals.h>
+
+json_object* llamapun_get_ngrams_old (xmlDocPtr doc);
+
