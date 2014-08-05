879774bdb7d3a26c2cbb169aa022620e2c112e92
jfschaefer <j.schaefer@jacobs-university.de>
2014-08-05 15:14:20 +0200

    more work on preprocessor

--- clib/preprocessor.c
+++ clib/preprocessor.c
@@ -43,10 +43,17 @@ void load_tag_repl_hash() {
 	if (tag_norm_handle) return;   //it's loaded already
 	struct hash_item_tag_normalization * tmp;
 	ADD_TAG_NORMALIZATION("math", "[MathFormula]");
-	ADD_TAG_NORMALIZATION("table", "")
+	//ADD_TAG_NORMALIZATION("table", "")
 	ADD_TAG_NORMALIZATION("script", "");
 	ADD_TAG_NORMALIZATION("title", "");
 	ADD_TAG_NORMALIZATION("head", "");
+	ADD_TAG_NORMALIZATION("bibliography", "");
+	ADD_TAG_NORMALIZATION("abstract", "");
+	ADD_TAG_NORMALIZATION("toctitle", "");
+	ADD_TAG_NORMALIZATION("creator", "");
+	ADD_TAG_NORMALIZATION("keywords", "");
+	ADD_TAG_NORMALIZATION("classification", "");
+	ADD_TAG_NORMALIZATION("acknowledgements", "");
 }
 
 #undef ADD_TAG_NORMALIZATION
@@ -60,7 +67,7 @@ char *getNormalization(const char *tag_name) {
 
 
 //=======================================================
-//Section: Core functionality
+//Section: Core preprocessing functionality
 //=======================================================
 
 void append_to_plaintext(struct tmp_preprocessing_data *tpd, const char *string) {
@@ -118,6 +125,7 @@ void remove_offset_attributes_actual(xmlNode *n) {
 				xmlFreeProp(attr);
 			}
 		}
+		remove_offset_attributes_actual(node->children);
 	}
 }
 
@@ -169,4 +177,80 @@ void preprocessorCleanUp() {
 		}
 		tag_norm_handle = NULL;
 	}
+}
+
+
+
+//=======================================================
+//Section: Fancy features
+//=======================================================
+
+
+xmlNode *getLowestSurroundingNode_actual(xmlNode *n, size_t offset_start, size_t offset_end) {
+	xmlNode *node;
+	xmlNode *tmpnode;
+	xmlAttr *attr;
+	xmlChar *tmpxmlstr;
+	size_t tmp_int;
+	int tags_found_info;
+	for (node = n; node!=NULL; node = node->next) {
+		tags_found_info = 0;  //reset info
+		for (attr = node->properties; attr != NULL; attr = attr->next) {
+			if (xmlStrEqual(attr->name, BAD_CAST "llamapun_offset_start")) {
+				tags_found_info |= 1;    //found offset_start
+				tmpxmlstr = xmlNodeGetContent(attr->children);
+				CHECK_ALLOC(tmpxmlstr);
+				sscanf((char *)tmpxmlstr, "%lu", &tmp_int);
+				if (tmp_int <= offset_start) tags_found_info |= 2;   //offset_start matches
+				xmlFree(tmpxmlstr);
+			} else if (xmlStrEqual(attr->name, BAD_CAST "llamapun_offset_end")) {
+				tags_found_info |= 4;   //found offset_end
+				tmpxmlstr = xmlNodeGetContent(attr->children);
+				CHECK_ALLOC(tmpxmlstr);
+				sscanf((char *)tmpxmlstr, "%lu", &tmp_int);
+				if (tmp_int >= offset_end) tags_found_info |= 8;   //offset_end matches
+			}
+			if ((tags_found_info & 1) && (tags_found_info & 4)) break;  //found all
+		}
+		//if (!((tags_found_info & 1) && (tags_found_info & 4))) {  //not all offsets are marked
+		//	fprintf(stderr, "Warning: Offsets are not marked\n");
+		//}
+		if ((tags_found_info & 2) && (tags_found_info & 8)) {  //node surrounds given offsets
+			tmpnode = getLowestSurroundingNode_actual(node->children, offset_start, offset_end);
+			if (tmpnode != NULL) return tmpnode;   //found lower surrounding node
+			else return node;
+		}
+	}
+	return NULL;   //haven't found any surrounding node
+}
+
+xmlNode *getLowestSurroundingNode(xmlDocPtr doc, size_t offset_start, size_t offset_end) {
+	xmlNode *node = xmlDocGetRootElement(doc);
+	return getLowestSurroundingNode_actual(node, offset_start, offset_end);
+}
+
+
+long getAttrIntVal(xmlNode *node, xmlChar *attr, long defaultvalue) {
+	xmlChar *xmlstr;
+	xmlAttr *a;
+	long result = defaultvalue;
+	for (a = node->properties; a!=NULL; a=a->next) {
+		if (xmlStrEqual(a->name, attr)) {  //found attribute
+			xmlstr = xmlNodeGetContent(a->children);
+			sscanf((char *)xmlstr, "%ld", &result);
+		}
+	}
+	return result;
+}
+
+xmlNode *insertNodeAroundOffsets(xmlDocPtr doc, size_t offset_start, size_t offset_end) {
+	/* UNFINISHED!!! */
+	xmlNode *node = xmlDocGetRootElement(doc);
+	xmlNode *surrounder = getLowestSurroundingNode_actual(node, offset_start, offset_end);
+	if (surrounder == NULL) return NULL;    //didn't find surrounding node
+	
+	long surr_start = getAttrIntVal(node, BAD_CAST "llamapun_offset_start", -1);
+	long surr_end   = getAttrIntVal(node, BAD_CAST "llamapun_offset_end", -1);
+	//return insertNodeAroundOffsets_actual(node, offset_start, offset_end);
+	return NULL;
 }
--- clib/preprocessor.h
+++ clib/preprocessor.h
@@ -2,3 +2,6 @@
 
 char * preprocess(xmlDocPtr doc);
 void remove_offset_attributes(xmlDocPtr doc);
+void preprocessorCleanUp();
+
+xmlNode *getLowestSurroundingNode(xmlDocPtr doc, size_t offset_start, size_t offset_end);
