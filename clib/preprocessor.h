fbadac7487a8da358213ef523979c8069cdec37f
jfschaefer <j.schaefer@jacobs-university.de>
2014-08-11 17:15:47 +0200

    removed preprocessor => will start again with better approach

--- clib/preprocessor.h
+++
@@ -1,7 +0,0 @@
-#include <libxml/tree.h>
-
-char * preprocess(xmlDocPtr doc);
-void remove_offset_attributes(xmlDocPtr doc);
-void preprocessorCleanUp();
-
-xmlNode *getLowestSurroundingNode(xmlDocPtr doc, size_t offset_start, size_t offset_end);
