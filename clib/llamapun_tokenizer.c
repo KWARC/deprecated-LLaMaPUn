97173fa7789076c8c462231cde34ec8b6294abda
jfschaefer <j.schaefer@jacobs-university.de>
2014-08-14 11:49:39 +0200

    added skeleton for tokenizer

---
+++ clib/llamapun_tokenizer.c
@@ -0,0 +1,34 @@
+//After installation of llamapun it can be compiled by
+//gcc llamapun_tokenizer.c -o tokenizer -lxml2 -lllamapun -I/usr/include/libxml2
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+#include "dnmlib.h"
+
+int main(void) {
+	//load example document
+	xmlDocPtr mydoc = xmlReadFile("../t/documents/1311.0066.xhtml", NULL, 0);
+
+	//Create DNM, with normalized math tags, and ignoring cite tags
+	dnmPtr mydnm = createDNM(mydoc, DNM_NORMALIZE_MATH | DNM_SKIP_CITE | DNM_EXTEND_PARA_RANGE);
+
+	printf("Plaintext: %s", mydnm->plaintext);
+
+
+	//have some arrays of offsets
+
+	size_t *offset_starts_array;
+	size_t *offset_ends_array;
+	//last argument is number of sentences
+	markSentences(mydnm, offset_starts_array, offset_ends_array, 0);	
+
+	//clean up
+	freeDNM(mydnm);
+	xmlFreeDoc(mydoc);
+	xmlCleanupParser();
+	
+	return 0;
+}
