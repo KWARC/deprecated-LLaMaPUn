6fada4bf564c6993cf90964a3567470caacf1c06
jfschaefer <j.schaefer@jacobs-university.de>
2014-08-25 00:40:25 +0200

    create read_document trying both, html5 and xhtml

--- clib/llamapun_utils.c
+++ clib/llamapun_utils.c
@@ -2,9 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-// JSON
-//#include <json-c/json.h>
-#include "jsoninclude.h"
+#include "llamapun_utils.h"
+
 
 char* cortex_stringify_response(json_object* response, size_t *size) {
   const char* string_response = json_object_to_json_string(response);
@@ -21,3 +20,45 @@ json_object* cortex_response_json(char *annotations, char *message, int status) 
   json_object_object_add(response,"log", json_log);
   json_object_object_add(response,"status", json_status);
   return response; }
+
+
+xmlDocPtr read_document(const char* filename) {
+  //read file
+  char *content;
+  size_t size;
+  FILE *file = fopen(filename, "rb");
+  if (file == NULL) {
+    fprintf(stderr, "Couldn't open %s\n", filename);
+    return NULL;
+  }
+  fseek(file, 0, SEEK_END);
+  size = ftell(file);
+  fseek(file, 0, SEEK_SET);
+  content = (char *) malloc(sizeof(char) * (size + 1));
+  fread(content, sizeof(char), size, file);
+  fclose(file);
+  content[size] = '\0';
+  
+/* the following is copied and adapted from
+   https://github.com/KWARC/mws/blob/master/src/crawler/parser/XmlParser.cpp#L65 */
+  xmlDocPtr doc;
+  // Try as XHTML
+  doc = xmlReadMemory(content, size, /* URL = */ "", "UTF-8",
+                            XML_PARSE_NOWARNING | XML_PARSE_NOERROR);
+  if (doc != NULL) {
+    return doc;
+  }
+  // Try as HTML
+  htmlParserCtxtPtr htmlParserCtxt = htmlNewParserCtxt();
+  doc = htmlCtxtReadMemory(
+          htmlParserCtxt, content, size, /* URL = */ "", "UTF-8",
+          HTML_PARSE_RECOVER | HTML_PARSE_NOWARNING | HTML_PARSE_NOERROR);
+  htmlFreeParserCtxt(htmlParserCtxt);
+  if (doc != NULL) {
+    return doc;
+  }
+
+  //if nothing works
+  fprintf(stderr, "Couldn't parse %s\n", filename);
+  return NULL;
+}
--- clib/llamapun_utils.h
+++ clib/llamapun_utils.h
@@ -1,5 +1,17 @@
 /* CorTeX utility functions and constants */
 
+
+#ifndef LLAMAPUN_UTILS_H
+#define LLAMAPUN_UTILS_H
+
+#include "jsoninclude.h"
+#include <libxml/parser.h>
+#include <libxml/HTMLparser.h>
+
 char* cortex_stringify_response(json_object* response, size_t *size);
 
 json_object* cortex_response_json(char *annotations, char *message, int status);
+
+xmlDocPtr read_document(const char* filename);
+
+#endif
