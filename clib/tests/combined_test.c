3398e9cbe40e07ab93cb268977f5656e755496e3
jfschaefer <j.schaefer@jacobs-university.de>
2014-08-24 18:26:34 +0200

    added small test for DOM unicode normalization

---
+++ clib/tests/combined_test.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "string.h"
+#include "../jsoninclude.h"
+#include "../dnmlib.h"
+#include "../unicode_normalizer.h"
+
+
+const char TEST_XHTML_DOCUMENT[] = "../../t/documents/1311.0066.xhtml";
+
+
+int is_ASCII(unsigned char *string) {
+  /* returns 1 if string contains only ascii characters, otherwise 0 */
+  while (*string) {
+    if (*string++ > 127) {
+      return 0;
+    }
+  }
+  return 1;   //didn't find any non-ascii character
+}
+
+
+int main(void) {
+  xmlDoc* doc = xmlReadFile(TEST_XHTML_DOCUMENT, NULL, 0);
+  if (doc == NULL) {
+    fprintf(stderr,"Test XHTML document could not be parsed, failing");
+    return 1; }
+  unicode_normalize_dom(doc);
+  dnmPtr dnm = createDNM(doc, DNM_NORMALIZE_TAGS);
+  if (strlen(dnm->plaintext) < 100) {   //something could have gone wrong...
+    fprintf(stderr, "The plaintext seems to be way too short\n");
+    return 1;
+  }
+  if (!is_ASCII((unsigned char *)dnm->plaintext)) {
+    fprintf(stderr, "The unicode-normalized document contains non-ascii characters\n");
+    return 1;
+  }
+  freeDNM(dnm);
+  xmlFreeDoc(doc);
+  xmlCleanupParser();
+  return 0;
+}
--- clib/tests/ngrams_test.c
+++ clib/tests/ngrams_test.c
@@ -5,8 +5,6 @@
 // XML DOM and XPath
 #include <libxml/tree.h>
 #include <libxml/parser.h>
-#include <libxml/xpath.h>
-#include <libxml/xpathInternals.h>
 
 #include "../llamapun_ngrams.h"
 
