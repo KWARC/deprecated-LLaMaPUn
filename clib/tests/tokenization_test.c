263ec0659e93461b758e1c8f0a23f1141925e885
dginev <d.ginev@jacobs-university.de>
2014-08-28 01:01:58 +0200

    using stopwords in sentence segmentation; reindent tokenization sources

--- clib/tests/tokenization_test.c
+++ clib/tests/tokenization_test.c
@@ -6,21 +6,21 @@
 #include <libxml/tree.h>
 
 int main(void) {
-        //load example document
-        fprintf(stderr, "\n\n Tokenization tests\n\n");
-        xmlDocPtr mydoc = xmlReadFile("../../t/documents/1311.0066.xhtml", NULL, XML_PARSE_RECOVER | XML_PARSE_NONET);
-        if (mydoc == NULL) { return 1;}
-        //Create DNM, with normalized math tags, and ignoring cite tags
-        dnmPtr mydnm = createDNM(mydoc, DNM_NORMALIZE_TAGS);
-        if (mydnm == NULL) { return 1;}
+  //load example document
+  fprintf(stderr, "\n\n Tokenization tests\n\n");
+  xmlDocPtr mydoc = xmlReadFile("../../t/documents/1311.0066.xhtml", NULL, XML_PARSE_RECOVER | XML_PARSE_NONET);
+  if (mydoc == NULL) { return 1;}
+  //Create DNM, with normalized math tags, and ignoring cite tags
+  dnmPtr mydnm = createDNM(mydoc, DNM_NORMALIZE_TAGS);
+  if (mydnm == NULL) { return 1;}
 
-        dnmRanges sentences = tokenize_sentences(mydnm->plaintext);
+  dnmRanges sentences = tokenize_sentences(mydnm->plaintext);
 
-        //clean up
-        free(sentences.range);
-        freeDNM(mydnm);
-        xmlFreeDoc(mydoc);
-        xmlCleanupParser();
+  //clean up
+  free(sentences.range);
+  freeDNM(mydnm);
+  xmlFreeDoc(mydoc);
+  xmlCleanupParser();
 
-        return 0;
+  return 0;
 }
