f884314dec865a492e8d4a7e3b15c9b0a2431c78
Deyan Ginev <d.ginev@jacobs-university.de>
2014-08-31 01:28:55 +0200

    TF_IDF progress: tokenization to words, refined XPaths for paras, documentation

--- src/tasks/gen_TF_IDF.c
+++ src/tasks/gen_TF_IDF.c
@@ -8,13 +8,16 @@
 
 #include "llamapun/utils.h"
 #include "llamapun/dnmlib.h"
+#include "llamapun/tokenizer.h"
 #include "llamapun/unicode_normalizer.h"
 int file_counter=0;
-xmlChar *paragraph_xpath = (xmlChar*) "//*[local-name()='div' and @class='ltx_para']";
+xmlChar *paragraph_xpath = (xmlChar*) "//*[local-name()='section' and @class='ltx_section']//*[local-name()='div' and @class='ltx_para']";
+xmlChar *relaxed_paragraph_xpath = (xmlChar*) "//*[local-name()='div' and @class='ltx_para']";
 
 int process_file(const char *filename, const struct stat *status, int type) {
-  if (file_counter > 100) { return 0; } // Limit for development
+  if (file_counter > 99 ) { return 0; } // Limit for development
   if (type != FTW_F) return 0; //Not a file
+  fprintf(stderr, " Loading %s\n",filename);
   xmlDoc *doc = read_document(filename);
   if (doc == NULL) return 0;   //error message printed by read_document
   /* 1. Normalize Unicode */
@@ -26,27 +29,48 @@ int process_file(const char *filename, const struct stat *status, int type) {
     xmlFreeDoc(doc);
     exit(1); }
   xmlXPathObjectPtr paragraphs_result = xmlXPathEvalExpression(paragraph_xpath,xpath_context);
-  if (paragraphs_result == NULL) { // Nothing to do if there's no math in the document
-    return 0; }
+  if ((paragraphs_result == NULL) || (paragraphs_result->nodesetval == NULL) || (paragraphs_result->nodesetval->nodeNr == 0)) { // Nothing to do if there's no math in the document
+    // Try the relaxed version: document isn't using LaTeX's \section{}s, maybe it's TeX.
+    paragraphs_result = xmlXPathEvalExpression(relaxed_paragraph_xpath,xpath_context);
+    if ((paragraphs_result == NULL) || (paragraphs_result->nodesetval == NULL) || (paragraphs_result->nodesetval->nodeNr == 0)) {
+      // We're really giving up here, probably empty document
+      return 0; } }
   xmlNodeSetPtr paragraph_nodeset = paragraphs_result->nodesetval;
   int para_index;
-  fprintf(stderr, "Found %d paragraphs\n", paragraph_nodeset->nodeNr);
+  /* Iterate over each paragraph: */
   for (para_index=0; para_index < paragraph_nodeset->nodeNr; para_index++) {
     xmlNodePtr paragraph_node = paragraph_nodeset->nodeTab[para_index];
-    // TODO: Continue here once createDNM works on libxml Nodes
-//    dnmPtr paragraph_dnm = create_DNM(paragraph_node, DNM_SKIP_TAGS);
-//    if (paragraph_dnm == NULL) {
-//      fprintf(stderr, "Couldn't create DNM for paragraph %d in document %s\n",para_index, filename);
-//      exit(1);
-//    }
-//    free_DNM(paragraph_dnm);
+    // Obtain NLP-friendly plain-text of the paragraph:
+    // -- We want to skip tags, as we only are interested in word counts for terms in TF-IDF
+    dnmPtr paragraph_dnm = create_DNM(paragraph_node, DNM_SKIP_TAGS);
+    if (paragraph_dnm == NULL) {
+      fprintf(stderr, "Couldn't create DNM for paragraph %d in document %s\n",para_index, filename);
+      exit(1);
+    }
+    /* 3. For every paragraph, tokenize sentences: */
+    char* paragraph_text = paragraph_dnm->plaintext;
+    dnmRanges sentences = tokenize_sentences(paragraph_text);
+    if (sentences.length == 0) {
+      continue; // No sentences, next paragraph.
+    }
+    //display_ranges(sentences,paragraph_text);
+    /* 4. For every sentence, tokenize words */
+    int sentence_index = 0;
+    for (sentence_index = 0; sentence_index < sentences.length; sentence_index++) {
+      dnmRanges words = tokenize_words(sentences.range[sentence_index], paragraph_text);
+      //display_ranges(words,paragraph_text);
+      free(words.range);
+    }
+
+    free(sentences.range);
+    free_DNM(paragraph_dnm);
     // We have the paragraph text, tokenize and obtain words:
   }
   free(paragraphs_result->nodesetval->nodeTab);
   free(paragraphs_result->nodesetval);
   xmlFree(paragraphs_result);
   xmlFreeDoc(doc);
-  fprintf(stderr,"DNM %d\n",file_counter++);
+  fprintf(stderr,"Completed document #%d\n",file_counter++);
   return 0;
 }
 
@@ -70,7 +94,11 @@ int main(int argc, char *argv[]) {
       exit(1);
   }
 
+  initialize_tokenizer("../../../third-party/senna/");
+
   ftw(source_directory, process_file, 1);
+
+  free_tokenizer();
   xmlCleanupParser();
   return 0;
 }
