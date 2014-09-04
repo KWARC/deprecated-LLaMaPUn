a7515fee79574451283fb7583fa187cbfc57480f
Deyan Ginev <d.ginev@jacobs-university.de>
2014-09-04 14:17:37 +0200

    ugh, rebase missed these

--- src/llamapun/language_detection.c
+++ src/llamapun/language_detection.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "file_paths.h"
+#include "local_paths.h"
 
 //a bit hacked, but it works:
 #include <textcat.c>
--- src/scripts/set_file_path.sh
+++ src/scripts/set_file_path.sh
@@ -2,5 +2,5 @@
 
 #determines the path of the repository root file and writes it into a header file
 
-pwd | sed -e 's/^\(.*\)\/src\/scripts$/#define ROOT_PATH \"\1\/\"/' > ../llamapun/local_paths.h
+pwd | sed -e 's/^\(.*\)\/src\/scripts$/#define LLAMAPUN_ROOT_PATH \"\1\/\"/' > ../llamapun/local_paths.h
 pwd | sed -e 's/^\(.*\)\/src\/scripts$/#define LIBTEXT_LANGUAGES_PATH \"\1\/third-party\/libtextcat-2.2\/langclass\/LM\/\"/' >> ../llamapun/local_paths.h
